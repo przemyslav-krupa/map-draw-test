43072648ba38f8af3279820a43933ffef357b548
przemek <przemyslav.krupa@gmail.com>
2023-11-12 17:15:32 +0100

    removed bevel, doesn't work (too many vertices)

--- lib/generators.dart
+++ lib/generators.dart
@@ -13,10 +13,8 @@ List<double> generateVerticesForPath(Path path) {
     Vector2 b = Vector2(
         x: (path.points[i + 1].x - 16.8) * scale,
         y: (51.2 - path.points[i + 1].y) * scale);
-    Vector2? c = i < path.points.length - 2 ? Vector2(
-        x: (path.points[i + 2].x - 16.8) * scale,
-        y: (51.2 - path.points[i + 2].y) * scale) : null;
-    result.addAll(generateLine(a, b, c, path.thickness).expand(expandPoints));
+    result
+        .addAll(generateLine(a, b, path.thickness * 0.1).expand(expandPoints));
   }
   return result;
 }
@@ -26,8 +24,7 @@ Iterable<double> expandPoints(Vector2 point) sync* {
   yield point.y;
 }
 
-List<Vector2> generateLine(Vector2 a, Vector2 b, Vector2? c, int thicknessValue) {
-  final thickness = thicknessValue * 0.1;
+List<Vector2> generateLine(Vector2 a, Vector2 b, double thickness) {
   final result = <Vector2>[];
   final perp1a = Vector2(x: a.y - b.y, y: b.x - a.x);
   final perp1b = Vector2(x: b.y - a.y, y: a.x - b.x);
@@ -43,23 +40,6 @@ List<Vector2> generateLine(Vector2 a, Vector2 b, Vector2? c, int thicknessValue)
   result.add(Vector2(x: a.x + p1bNormalized.x, y: a.y + p1bNormalized.y));
   result.add(Vector2(x: b.x + p1bNormalized.x, y: b.y + p1bNormalized.y));
   result.add(Vector2(x: b.x + p1aNormalized.x, y: b.y + p1aNormalized.y));
-  //create bevel
-  if(c != null){
-    final perp2a = Vector2(x: b.y - c.y, y: c.x - b.x);
-    final perp2b = Vector2(x: c.y - b.y, y: b.x - c.x);
-    final mag2a = sqrt(perp2a.x * perp2a.x + perp2a.y * perp2a.y);
-    final mag2b = sqrt(perp2b.x * perp2b.x + perp2b.y * perp2b.y);
-    final p2aNormalized =
-    Vector2(x: perp2a.x * thickness / mag2a, y: perp2a.y * thickness / mag2a);
-    final p2bNormalized =
-    Vector2(x: perp2b.x * thickness / mag2b, y: perp2b.y * thickness / mag2b);
-    result.add(Vector2(x: b.x + p1aNormalized.x, y: b.y + p1aNormalized.y));
-    result.add(Vector2(x: b.x + p2aNormalized.x, y: b.y + p2aNormalized.y));
-    result.add(b);
-    result.add(Vector2(x: b.x + p1bNormalized.x, y: b.y + p1bNormalized.y));
-    result.add(Vector2(x: b.x + p2bNormalized.x, y: b.y + p2bNormalized.y));
-    result.add(b);
-  }
 
   return result;
 }
